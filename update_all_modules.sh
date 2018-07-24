99b0f7b9173e36e07552b5372aa08b8d06a3f049
Jamie Alexandre <jamalex@gmail.com>
2018-07-24 14:40:52 -0700

    Remove the old upgrade module

--- update_all_modules.sh
+++ update_all_modules.sh
@@ -28,23 +28,13 @@ echo "UPDATE modules SET version='$version', logofilename='kolibri-logo.svg' WHE
 popd
 
 # update the upgrade module
-pushd multi-kolibri-upgrade
-cp * /var/modules/multi-kolibri-upgrade
-ln -s rachel-index.php /var/modules/multi-kolibri-upgrade/index.htmlf
-version=$(curl "https://launchpad.net/~learningequality/+archive/ubuntu/kolibri" 2> /dev/null | egrep "[0-9]+\.[0-9]+\.[0-9]+-[0-9]*ubuntu[0-9]*" -o | head -n 1)
-version="$version (`md5sum * 2> /dev/null | md5sum | cut -c1-6`)" # include a hash of the scripts to ensure we trigger an upgrade if those change too
-echo '<!-- version="'$version'" -->' > /var/modules/multi-kolibri-upgrade/rachel-index.php
-echo "UPDATE modules SET version='$version' WHERE moddir = 'multi-kolibri-upgrade';" | mysql rachelmods -u root
-popd
-
-# update the local upgrade module
 pushd zz-kolibri-upgrade
 cp * /var/modules/zz-kolibri-upgrade
-ln -s rachel-index.php /var/modules/zz-kolibri-upgrade/index.htmlf
 wget -O /var/modules/zz-kolibri-upgrade/kolibri.deb https://learningequality.org/r/kolibri-deb-latest
 version=$(dpkg-deb -I /var/modules/zz-kolibri-upgrade/kolibri.deb | egrep "[0-9]+\.[0-9]+\.[0-9]+-[0-9]*ubuntu[0-9]*" -o | head -n 1)
 version="$version (`md5sum * 2> /dev/null | md5sum | cut -c1-6`)" # include a hash of the scripts to ensure we trigger an upgrade if those change too
 echo '<!-- version="'$version'" -->' > /var/modules/zz-kolibri-upgrade/rachel-index.php
+ln -s rachel-index.php /var/modules/zz-kolibri-upgrade/index.htmlf
 echo "UPDATE modules SET version='$version' WHERE moddir = 'zz-kolibri-upgrade';" | mysql rachelmods -u root
 popd
 
