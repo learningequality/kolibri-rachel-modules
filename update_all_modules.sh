9abadb208c9754541ee31a57e3fb512fca78a708
Jamie Alexandre <jamalex@gmail.com>
2018-07-24 13:04:06 -0700

    Add local upgrade module

--- update_all_modules.sh
+++ update_all_modules.sh
@@ -30,7 +30,17 @@ pushd multi-kolibri-upgrade
 cp * /var/modules/multi-kolibri-upgrade
 version=$(curl "https://launchpad.net/~learningequality/+archive/ubuntu/kolibri" 2> /dev/null | egrep "[0-9]+\.[0-9]+\.[0-9]+-[0-9]*ubuntu[0-9]*" -o | head -n 1)
 version="$version (`md5sum * 2> /dev/null | md5sum | cut -c1-6`)" # include a hash of the scripts to ensure we trigger an upgrade if those change too
-echo '<!-- version="'$version'" -->' > rachel-index.php
+echo '<!-- version="'$version'" -->' > /var/modules/multi-kolibri-upgrade/rachel-index.php
+echo "UPDATE modules SET version='$version' WHERE moddir = 'multi-kolibri-upgrade';" | mysql rachelmods -u root
+popd
+
+# update the local upgrade module
+pushd multi-kolibri-upgrade-local
+cp * /var/modules/multi-kolibri-upgrade-local
+wget -O /var/modules/multi-kolibri-upgrade-local/kolibri.deb https://learningequality.org/r/kolibri-deb-latest
+version=$(curl "https://launchpad.net/~learningequality/+archive/ubuntu/kolibri" 2> /dev/null | egrep "[0-9]+\.[0-9]+\.[0-9]+-[0-9]*ubuntu[0-9]*" -o | head -n 1)
+version="$version (`md5sum * 2> /dev/null | md5sum | cut -c1-6`)" # include a hash of the scripts to ensure we trigger an upgrade if those change too
+echo '<!-- version="'$version'" -->' > /var/modules/multi-kolibri-upgrade-local/rachel-index.php
 echo "UPDATE modules SET version='$version' WHERE moddir = 'multi-kolibri-upgrade';" | mysql rachelmods -u root
 popd
 
